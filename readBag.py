a18285a71a3d0f02393f75be7254d14d9e2eb12c
MattyD797 <mattduggan2018@gmail.com>
2022-07-01 04:38:37 -0400

    creating one SQl:errors still unresolved in csv 1 & 2

--- readBag.py
+++ readBag.py
@@ -8,17 +8,30 @@ import numpy as np
 import multiprocessing as mp
 import sqlite3
 from pathlib import Path
+import rosbag
 
 b=bagreader('bags/blF1.bag')
 
-
 Path('my_data.db').touch()
-conn = sqlite3.connect('my_data.db')
-c = conn.cursor
+conn = sqlite3.connect('my_data.sqlite')
+c = conn.cursor()
+
+#TODO: finish creating table in SQL
+#Need data types of each column
+#Error in csv 1 & 2: doesn't start collecting all data until row 8
+#copies column R-V in csv #2 and strange error in csv#1 as well
+c.execute('DROP TABLE IF EXISTS my_data.sqlite')
+c.execute('''
+CREATE TABLE "my_data"(
+    "Time" REAL,
+    "" 
+)
+''')
 
 csvfiles = []
 for t in b.topics:
+    print(t)
     data = b.message_by_topic(t)
+    
     csvfiles.append(data)
-
-
+    
